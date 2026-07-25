f02b053332dc12ebc0ff170c55f074fdd2ce10b9
Arun Upadhyay <arunk7426@gmail.com>
2026-07-25 16:42:20 +0530

    Day 2: Worker model, admin panel, static setup and homepage

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'workers'
+    'workers',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,4 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=BASE_DIR/"staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
--- workers/models.py
+++ workers/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Worker(models.Model):
+    PROFESSION_CHOICES = [
+        ("Plumber", "Plumber"),
+        ("Electrician", "Electrician"),
+        ("Carpenter", "Carpenter"),
+        ("Painter", "Painter"),
+        ("Mason", "Mason"),
+        ("Welder", "Welder"),
+        ("Cleaner", "Cleaner"),
+        ("Driver", "Driver"),
+        ("Other", "Other"),
+    ]
+
+    name = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=15)
+    profession = models.CharField(max_length=30, choices=PROFESSION_CHOICES)
+    experience = models.PositiveIntegerField(help_text="Experience in years")
+    city = models.CharField(max_length=100)
+    area = models.CharField(max_length=100)
+    daily_wage = models.DecimalField(max_digits=8, decimal_places=2)
+    about = models.TextField(blank=True)
+    available = models.BooleanField(default=True)
+    profile_photo = models.ImageField(upload_to="workers/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
